ff42ea2bac0bbbd1fafdd8a3c172d489777cd78d
rahatkabir007 <rahatkabir04@gmail.com>
2024-11-03 23:18:27 +0600

    render

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { ToastContainer } from "react-toastify";
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import { store, persistor } from './store';
@@ -14,17 +13,6 @@ import { HTML5Backend } from 'react-dnd-html5-backend';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
-    <ToastContainer
-      position="top-right"
-      autoClose={2000}
-      hideProgressBar={false}
-      newestOnTop={false}
-      closeOnClick
-      rtl={false}
-      pauseOnFocusLoss
-      draggable
-      pauseOnHover
-    />
     <PersistGate loading={null} persistor={persistor}>
       <DndProvider backend={HTML5Backend}>
         <App />
--- src/utils/Jsondata.js
+++
@@ -1,6 +0,0 @@
-
-export class Jsondata {
-    static categories = [
-
-    ]
-}
